19647015bf96bdd2f48ea6fe23d6eaaa92fbf608
kabir-coderex <kabir@coderex.co>
2025-08-23 18:28:07 +0600

    started the server

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from 'express';
+
+const app : Application = express();
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`
+        <h1>Welcome to my Library Management System</h1>
+        <p>
+            Please visit here for documentation: 
+            <a href="https://github.com/Humayun63/library-mangement" target="_blank">
+                GitHub Repository
+            </a>
+        </p>
+    `);
+})
+
+export default app;
