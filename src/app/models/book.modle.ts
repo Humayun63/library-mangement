7e33f871298cada3fc03c53932b894bd62f49941
kabir-coderex <kabir@coderex.co>
2025-08-23 19:11:01 +0600

    schema created for book

---
+++ src/app/models/book.modle.ts
@@ -0,0 +1,35 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "../interfaces/book.interface";
+
+const bookSchema = new Schema<IBook>({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    }, 
+    isbn: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    available: {
+        type: Boolean,
+        required: true,
+    },
+    copies: {
+        type: Number,
+        required: true
+    }
+});
+
+export const Book = model<IBook>('Book', bookSchema);
