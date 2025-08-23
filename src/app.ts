29ebf4f439871de3deb66625a361037afac9c12f
kabir-coderex <kabir@coderex.co>
2025-08-23 19:04:12 +0600

    interface created for book

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import express, { Application, Request, Response } from 'express';
 const app : Application = express();
 app.use(express.json());
 
+// app.use('/api/books')
+
 app.get('/', (req: Request, res: Response) => {
     res.send(`
         <h1>Welcome to my Library Management System</h1>
