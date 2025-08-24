75ecb7fb4be382050bd245131e15cd4d1083155c
kabir-coderex <kabir@coderex.co>
2025-08-24 10:40:45 +0600

    added fallback for invalid routes

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,21 @@ app.get('/', (req: Request, res: Response) => {
     `);
 })
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    try{
+        res.status(404).json({
+            success: false,
+            message: "Route not found",
+            error: {
+                name: "InvalidRouteAccess",
+                path: req.originalUrl   
+            }
+    });
+    } catch (error){
+        next(error)
+    }
+});
+
 
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
     if(error){
